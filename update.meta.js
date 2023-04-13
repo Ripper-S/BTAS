89214f704a25eb7729f1331009963714ab63746e
ripper-s <ripper-s@ripper-s.com>
2023-04-13 18:59:19 +0800

    add update.meta.js
    Signed-off-by: ripper-s <ripper-s@ripper-s.com>

---
+++ update.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         BTAS
+// @namespace    http://tampermonkey.net/
+// @version      1.0.2
+// @description  Blue Team Assistance Script
+// @author       Barry Y Yang; Jack SA Chen
+// @updateURL    https://github.com/Ripper-S/BTAS/blob/main/BTAS.js
+// @downloadURL  https://github.com/Ripper-S/BTAS/blob/main/BTAS.js
+// @match        https://caas.pwchk.com/*
+// @icon         https://www.google.com/s2/favicons?domain=pwchk.com
+// @require      https://code.jquery.com/jquery-3.6.4.min.js
+// @grant        GM_registerMenuCommand
+// @grant        GM_unregisterMenuCommand
+// ==/UserScript==
