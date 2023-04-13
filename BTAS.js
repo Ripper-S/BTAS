8c9a583561a184f21b3ea238fee8a7ffed8ee68e
ripper-s <ripper-s@ripper-s.com>
2023-04-13 20:42:09 +0800

    BTAS 1.0.3 add @license
    Signed-off-by: ripper-s <ripper-s@ripper-s.com>

--- BTAS.js
+++ BTAS.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         BTAS
 // @namespace    http://tampermonkey.net/
-// @version      1.0.2
+// @version      1.0.3
 // @description  Blue Team Assistance Script
 // @author       Barry Y Yang; Jack SA Chen
-// @require      https://raw.githubusercontent.com/Ripper-S/BTAS/main/update.meta.js
+// @license      Apache-2.0
 // @match        https://caas.pwchk.com/*
 // @icon         https://www.google.com/s2/favicons?domain=pwchk.com
 // @require      https://code.jquery.com/jquery-3.6.4.min.js
