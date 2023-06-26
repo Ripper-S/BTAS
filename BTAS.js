acdc85aad6e72fb475a96a50417e4c901d628164
Xingyu <laysone2020@gmail.com>
2023-06-27 07:46:41 +0800

    add reputation search menu

--- BTAS.js
+++ BTAS.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         BTAS
+// @name         BTAS-Beta
 // @namespace    https://github.com/Ripper-S/BTAS
 // @homepageURL  https://github.com/Ripper-S/BTAS
 // @version      1.3.2
-// @description  Blue Team Assistance Script
+// @description  BTAS Beta Release Channel
 // @author       Barry Y Yang; Jack SA Chen; Xingyu X Zhou
 // @license      Apache-2.0
 // @updateURL    https://greasyfork.org/scripts/463908-btas/code/BTAS.user.js
@@ -47,6 +47,7 @@ function registerSearchMenu() {
                 '%22Log%20Source%20Domain%22%20~%20%22%D%22%20' +
                 'ORDER%20BY%20created%20DESC'
         },
+        { name: 'Reputation', url: 'https://172.18.2.23/instance/execute/reputation/ip?ip=%s' },
         { name: 'VT', url: 'https://www.virustotal.com/gui/search/%s' },
         { name: 'AbuseIPDB', url: 'https://www.abuseipdb.com/check/%s' }
     ];
